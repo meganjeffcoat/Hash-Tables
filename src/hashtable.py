95f15284e217373a300e138aa16b8540fe55c678
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-09-30 15:18:04 -0500

    can't add count, starting over

--- src/hashtable.py
+++ src/hashtable.py
@@ -15,7 +15,6 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-        self.count = 0
 
 
     def _hash(self, key):
@@ -85,11 +84,7 @@ class HashTable:
 
         Fill this in.
         '''
-        self.capacity *= 2
-        new_storage = [None] * self.capacity
-        for i in range(self.count):
-            new_storage[i] = self.storage[i]
-        self.storage = new_storage
+        pass
 
 
 
