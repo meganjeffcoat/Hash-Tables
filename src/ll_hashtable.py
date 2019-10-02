d4f1aaa8cbf8f21498254eb3acb5082a6388b7fb
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-01 22:00:13 -0500

    went back to where I was at before and fixed orginal errors Tests Pass MVP Baby

--- src/ll_hashtable.py
+++ src/ll_hashtable.py
@@ -1,40 +1,38 @@
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-        self.count = 0
-
 
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
-
         OPTIONAL STRETCH: Research and implement DJB2
         '''
         pass
-
 
     def _hash_mod(self, key):
         '''
@@ -43,60 +41,43 @@ class HashTable:
         '''
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
         '''
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Fill this in.
         '''
-        
-        if self.count >= self.capacity:
-            self.resize()
-
         index = self._hash_mod(key)
         if self.storage[index] is not None:
-            # print("Warning: COLLISION")
             curr = self.storage[index]
+
             while curr.next is not None and curr.key != key:
                 curr = curr.next
-            curr.next = LinkedPair(key, value)
-            return
+            if curr.key == key:
+                curr.value = value
+                return
+            else:
+                curr.next = LinkedPair(key, value)
+                return
         else:
             self.storage[index] = LinkedPair(key, value)
-
 
     def remove(self, key):
         '''
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Fill this in.
         '''
-        # index = self._hash_mod(key)
-
-        # if self.storage[index] is None:
-        #     print("Warning: key not found")
-        #     return
-        # self.storage[index] = None
-        self.count -= 1
-
         index = self._hash_mod(key)
         if self.storage[index] == None:
-            print(f" {key} is not on this table")
+            print('Warning')
         else:
-            self.storage[index] = None 
-
+            self.storage[index] = None
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Fill this in.
         '''
         index = self._hash_mod(key)
@@ -113,28 +94,20 @@ class HashTable:
                     curr = curr.next
             return None
 
-
     def resize(self):
         '''
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
         Fill this in.
         '''
-        
-        if self.count < self.capacity:
-            return
-
         self.capacity *= 2
-        new_storage = [None] * self.capacity
-        self.count = 0
-        for pair in self.storage:
-            if pair is not None:
-                new_index = self._hash_mod(pair.key)
-                new_storage[new_index] = pair
-
-        self.storage = new_storage
-        print("double size", self.capacity)
-
+        temp = list(self.storage)
+        self.storage = [None] * self.capacity
+        for i in [item for item in temp if item != None]:
+            curr = i
+            while curr is not None:
+                self.insert(curr.key, curr.value)
+                curr = curr.next
 
 
 if __name__ == "__main__":
@@ -150,9 +123,6 @@ if __name__ == "__main__":
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
     print(ht.retrieve("line_3"))
-
-    ht.remove("line_3")
-    # ht.remove("line_3") 
 
     # Test resizing
     old_capacity = len(ht.storage)
